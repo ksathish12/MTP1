4b258953dd96a37c9776e87a3048ce396dd7ea6b
ksathish12 <sathishk.a.12@gmail.com>
2016-07-05 12:03:26 +0530

    Update 1d_axisymmetric_with_non-uniform_k and NU heatsource_iterationmethod5.py

--- 1d_axisymmetric_with_non-uniform_k and NU heatsource_iterationmethod5.py
+++ 1d_axisymmetric_with_non-uniform_k and NU heatsource_iterationmethod5.py
@@ -3,49 +3,12 @@ from matplotlib import pyplot
 import time
 #import numba
 start_time = time.time()
-x_file = open('C:/Users/lenovo/Desktop/M.Tech Project/captelli.txt')
-data1= np.loadtxt(x_file)
-T_k=data1[:,0]
-k_T=data1[:,1]
-sig=data1[:,2]
-
-
-
-#This function is used to find the Thermal Conductivity values for specific Temperature
-#def T2K(t,T_k,k_T):
-#    if t>=20000:
-#        k = 0.0003269*(t-20000) + 3.225
-#    elif t<0:
-#        print 'Error: Temperature below zero'       
-#    else:
-#        for i in range(0,34):
-#            if T_k[i]<=t and t<T_k[i+1]:
-#                k =(k_T[i+1]*(t-T_k[i]) + k_T[i]*(T_k[i+1]-t))/(T_k[i+1] - T_k[i])
-#    return k
 
 
 def T2K(t,T_k,k_T):
     #k = 0.0002*t + 0.0457
     k=2e-25*t**6 - 2e-20*t**5 + 7e-16*t**4 - 1e-11*t**3 + 1e-07*t**2 + 6e-6*t    
     return k
-
-
-#This function is used to find Sigma values for specific Temperature
-#def T2S(t):
-#    if t>=4000:
-#        s=0.6637*(t-4000)
-#    elif t>=0 and t<4000:
-#        s=0
-#    else:
-#        print 'Error: Temperature below zero'
-#    return s   
-
-#def T2S(t):
-#    if t>=4000:
-#        s=0.6638*(t-4000)
-#    else:
-#        s=0
-#    return s
 
 def T2S(t):
     #s=0.6638*(t)
@@ -60,7 +23,7 @@ nr=500
 R=R2-R1
 
 E=127
-#q0=100000
+
 Tb=1500 # This is the boundary temperature
 
 #spacing
@@ -81,17 +44,8 @@ for i in range(0,nr):
     temp2=2*np.pi*dz*(R1+dr*(i+1))
     An=np.append(An,temp2)
 
-
-#As[0]=2*np.pi*dz*(R1+dr/4)
-#
-#An[nr-1]=2*np.pi*dz*(R1+dr*(nr-0.25))
-
-
 ## Numerical Solution
 # Initial assumption of T
-#T_i=Tb*np.ones(nr+1)
-#T_i=np.append(T_i,Tb)
-
 T_i=np.linspace(2*Tb,Tb,nr)
 T_i=np.append(2*Tb,T_i)
 T_i=np.append(T_i,Tb)
@@ -104,13 +58,7 @@ k_temp=np.zeros(nr+1)
 
 
 for i in range(0,500):
-    #print i
     T_temp=(T[1:]+T[:-1])/2
-    #pyplot.figure(4)    
-    ##plotting the values
-    #pyplot.plot(T_temp, 'bx')
-    #pyplot.show()
-    #pyplot.pause(0.03)
     for j in range(0,nr+1):
         k_temp[j]=T2K(T_temp[j],T_k,k_T)
     kn=k_temp[1:]
@@ -128,7 +76,7 @@ for i in range(0,500):
     
     for j in range(nr):
         su[j]=2*np.pi*(R1+j*dr/2)*dr*dz*T2S(T[j+1])*E**2
-        #su[j]=2*np.pi*(R1+j*dr/2)*dr*dz*q0
+
    
     #Defining constant for first exterior node 
     aS[0]=0
